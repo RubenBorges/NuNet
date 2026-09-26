ac7076319192a15970beb60aa535b0cef9f6b8eb
Ruben M Borges <rubenmborges@outlook.com>
2026-09-26 00:00:47 -0400

    Continue CNN v2 development

---
+++ build/src/CMakeFiles/NuNet.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/boopy/dev/Projects/NeuralNetwork/src/CNN.cpp" "src/CMakeFiles/NuNet.dir/CNN.cpp.o" "gcc" "src/CMakeFiles/NuNet.dir/CNN.cpp.o.d"
+  "/home/boopy/dev/Projects/NeuralNetwork/src/Conv2D.cpp" "src/CMakeFiles/NuNet.dir/Conv2D.cpp.o" "gcc" "src/CMakeFiles/NuNet.dir/Conv2D.cpp.o.d"
+  "/home/boopy/dev/Projects/NeuralNetwork/src/ConvNuNet.cpp" "src/CMakeFiles/NuNet.dir/ConvNuNet.cpp.o" "gcc" "src/CMakeFiles/NuNet.dir/ConvNuNet.cpp.o.d"
+  "/home/boopy/dev/Projects/NeuralNetwork/src/DenseLayer.cpp" "src/CMakeFiles/NuNet.dir/DenseLayer.cpp.o" "gcc" "src/CMakeFiles/NuNet.dir/DenseLayer.cpp.o.d"
+  "/home/boopy/dev/Projects/NeuralNetwork/src/Matrix.cpp" "src/CMakeFiles/NuNet.dir/Matrix.cpp.o" "gcc" "src/CMakeFiles/NuNet.dir/Matrix.cpp.o.d"
+  "/home/boopy/dev/Projects/NeuralNetwork/src/Tensor.cpp" "src/CMakeFiles/NuNet.dir/Tensor.cpp.o" "gcc" "src/CMakeFiles/NuNet.dir/Tensor.cpp.o.d"
+  "" "src/NuNet" "gcc" "src/CMakeFiles/NuNet.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
